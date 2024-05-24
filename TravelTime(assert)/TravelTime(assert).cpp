fcb4299cbde0c9ea12c2daf8dc04fc547c2baaf2
MkaGodh <fenryrvla@yandex.ru>
2024-05-24 12:21:38 +0300

    Добавьте файлы проекта.

---
+++ TravelTime(assert)/TravelTime(assert).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+float travelTime(float distance, float speed) {
+
+	assert(speed > 0.0f);
+	assert(distance > 0.0f);
+	return distance / speed;
+
+}
+
+int main() {
+	float distance;
+	float speed;
+
+	cout << "Please, input distance: "; cin >> distance;
+	cout << "Please, input speed: "; cin >> speed;
+
+	float time = travelTime(distance, speed);
+
+	cout << "Travel time: " << time << " hours" << endl;
+
+	return 0;
+
+}
